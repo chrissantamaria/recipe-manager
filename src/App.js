e2ee22774e557d3b613063127caa1ae0db931866
Chris Santamaria <chris@santamaria.me>
2020-04-25 19:09:59 -0400

    Move router to separate file

--- src/App.js
+++ src/App.js
@@ -1,37 +1,13 @@
 import React from 'react';
 import { registerRootComponent } from 'expo';
 import { Provider as PaperProvider } from 'react-native-paper';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-
-import HomeScreen from './components/Home';
-import RecipeScreen from './components/Recipe';
-
-const Stack = createStackNavigator();
+import Router from './Router';
 
 function App() {
   return (
-    <NavigationContainer>
-      <PaperProvider>
-        <Stack.Navigator initialRouteName="Home">
-          <Stack.Screen
-            name="Home"
-            component={HomeScreen}
-            options={{ title: 'Available Recipes' }}
-          />
-          <Stack.Screen
-            name="Recipe"
-            component={RecipeScreen}
-            options={({ route }) => ({
-              title: route.params.title,
-              // Forcing title instead of potentially "Available Recipes"
-              // depending on recipe title length
-              headerBackTitle: 'Back',
-            })}
-          />
-        </Stack.Navigator>
-      </PaperProvider>
-    </NavigationContainer>
+    <PaperProvider>
+      <Router />
+    </PaperProvider>
   );
 }
 
---
+++ src/Router.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import HomeScreen from './components/Home';
+import RecipeScreen from './components/Recipe';
+
+const Stack = createStackNavigator();
+
+export default function Router() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: 'Available Recipes' }}
+        />
+        <Stack.Screen
+          name="Recipe"
+          component={RecipeScreen}
+          options={({ route }) => ({
+            title: route.params.title,
+            // Forcing title instead of potentially "Available Recipes"
+            // depending on recipe title length
+            headerBackTitle: 'Back',
+          })}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
